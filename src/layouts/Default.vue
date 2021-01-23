a1cddeac3b94e2a058533dc212ed5d2516a80a75
Raul <raul.bethencourt.pro@gmail.com>
2021-01-23 21:23:10 +0100

    :lipstick: styling default layout

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,17 +1,49 @@
 <template>
-  <div class="layout">
-    <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
-      </nav>
-    </header>
-    <slot/>
-  </div>
+    <v-app>
+        <v-navigation-drawer permanent>
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-title class="title">
+                        Application
+                    </v-list-item-title>
+                    <v-list-item-subtitle> subtext </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+
+            <v-divider></v-divider>
+
+            <v-list dense nav>
+                <v-list-item v-for="item in items" :key="item.title" link>
+                    <v-list-item-icon>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-item-icon>
+
+                    <v-list-item-content>
+                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+        <v-main>
+            <slot />
+        </v-main>
+    </v-app>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            items: [
+                { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+                { title: 'Photos', icon: 'mdi-image' },
+                { title: 'About', icon: 'mdi-help-box' }
+            ],
+            right: null
+        }
+    }
+}
+</script>
 
 <static-query>
 query {
@@ -22,29 +54,9 @@ query {
 </static-query>
 
 <style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
+.v-application--wrap {
+    flex-direction: row;
 }
 
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
-.nav__link {
-  margin-left: 20px;
-}
+/* TODO 1:19:10 */
 </style>
