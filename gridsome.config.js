06fe1c03086ae8c3926d6b23e5f80fe5a110a92f
Raul <raul.bethencourt.pro@gmail.com>
2021-02-11 15:52:30 +0100

    :globe_with_meridians: adding I18n plugin for internasinalisation

--- gridsome.config.js
+++ gridsome.config.js
@@ -18,5 +18,31 @@ module.exports = {
     types.forEach(type => {
       addStyleResource(config.module.rule('scss').oneOf(type))
     })
-  }
+  },
+  plugins: [
+    {
+      use: "gridsome-plugin-i18n",
+      options: {
+        locales: [ // locales list
+          'es-es',
+          'fr-fr',
+          'en-gb'
+        ],
+        pathAliases: { // path segment alias for each locales
+          'es-es': 'es',
+          'fr-fr': 'fr',
+          'en-gb': 'en'
+        },
+        fallbackLocale: 'fr-fr', // fallback language
+        defaultLocale: 'fr-fr', // default language
+        enablePathRewrite: true, // rewrite path with locale prefix, default: true
+        rewriteDefaultLanguage: true, // rewrite default locale, default: true
+        messages: {
+          'es-es': require('./src/locales/es-es.json'), // Messages files
+          'fr-fr': require('./src/locales/fr-fr.json'),
+          'en-gb': require('./src/locales/en-gb.json'),
+        }
+      }
+    }
+  ]
 }
