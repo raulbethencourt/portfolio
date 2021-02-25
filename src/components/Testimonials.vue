f9384f21e70344b3dba1f71b91419e73e8c4a373
Raul <raul.bethencourt.pro@gmail.com>
2021-02-25 15:03:24 +0100

    :sparkles: add testimonials html struture, plus json data

--- src/components/Testimonials.vue
+++ src/components/Testimonials.vue
@@ -1,10 +1,40 @@
 <template>
     <section
-        class="skills-section"
-        :id="
-            `${$page.texts.edges[0].node.sidebar.items[5].title.toLowerCase()}`
-        "
+        class="testimonials-section"
+        :id="`${$page.texts.edges[0].node.sidebar.items[5].title.toLowerCase()}`"
     >
-        <h2 class="text-h2">Testimonials</h2>
+        <v-container>
+            <v-row>
+                <v-col cols="10" offset="1">
+                    <h2 class="text-h2 font-weight-bold mb-5">
+                        {{ $page.texts.edges[0].node.testimonials.title }}
+                    </h2>
+                </v-col>
+            </v-row>
+
+            <v-row>
+                <v-col
+                    v-for="(testimony, i) in $page.texts.edges[0].node
+                        .testimonials.items"
+                    :key="i"
+                    cols="12"
+                    md="4"
+                    offset-md="1"
+                    class="d-flex flex-column align-items-center text-center"
+                >
+                    <p
+                        v-for="(text, i) in testimony.texts"
+                        :key="i"
+                        class="mb-3"
+                    >
+                        &laquo; {{ text }} &raquo;
+                    </p>
+                    <p class="mb-5">
+                        {{ testimony.author }},
+                        <span>{{ testimony.company }}</span>
+                    </p>
+                </v-col>
+            </v-row>
+        </v-container>
     </section>
 </template>
