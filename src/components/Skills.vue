ebdc5d29871c7ef31e5cde62004fdf41acb186fd
Raul <raul.bethencourt.pro@gmail.com>
2021-03-18 15:13:45 +0100

    📱 finish skills section responsif

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -14,47 +14,40 @@
                 </v-col>
             </v-row>
 
-            <v-row class="mt-10">
-                <v-col cols="8" offset="2">
-                    <v-row>
-                        <v-col
-                            v-for="(logo, i) in $page.texts.edges[0].node.skills.logos"
-                            :key="i"
-                            cols="2"
-                            class="d-flex justify-center"
-                        >
-                            <v-tooltip top>
-                                <template v-slot:activator="{ on, attrs }">
-                                    <v-hover v-slot="{ hover }" open-delay="100">
-                                        <v-btn
-                                            fab
-                                            :href="logo.url"
-                                            target="_blank"
-                                            :elevation="hover ? 16 : 2"
-                                            :class="{ 'on-hover': hover } + 'mx-auto'"
-                                            v-bind="attrs"
-                                            v-on="on"
-                                            rel="noreferrer noopener"
-                                            width="100"
-                                            height="100"
-                                        >
-                                            <v-img
-                                                max-width="70"
-                                                max-height="70"
-                                                :src="
-                                                    require(`@/assets/images/logos/${
-                                                        logo.icon
-                                                    }.png`)
-                                                "
-                                                class="skill-logo"
-                                            ></v-img>
-                                        </v-btn>
-                                    </v-hover>
-                                </template>
-                                <span class="d-none">My tooltip</span>
-                            </v-tooltip>
-                        </v-col>
-                    </v-row>
+            <v-row class="mt-md-5">
+                <v-col cols="12" lg="8" offset-lg="2" class="d-flex flex-wrap justify-center">
+                    <div
+                        v-for="(logo, i) in $page.texts.edges[0].node.skills.logos"
+                        :key="i"
+                        class="skill__container"
+                    >
+                        <v-tooltip top>
+                            <template v-slot:activator="{ on, attrs }">
+                                <v-hover v-slot="{ hover }" open-delay="100">
+                                    <v-btn
+                                        fab
+                                        :href="logo.url"
+                                        target="_blank"
+                                        :elevation="hover ? 16 : 2"
+                                        :class="{ 'on-hover': hover } + 'mx-auto'"
+                                        v-bind="attrs"
+                                        v-on="on"
+                                        rel="noreferrer noopener"
+                                        :width="$vuetify.breakpoint.mdAndUp ? 100 : 70"
+                                        :height="$vuetify.breakpoint.mdAndUp ? 100 : 70"
+                                    >
+                                        <v-img
+                                            :max-width="$vuetify.breakpoint.mdAndUp ? 70 : 40"
+                                            :max-height="$vuetify.breakpoint.mdAndUp ? 70 : 40"
+                                            :src="require(`@/assets/images/logos/${logo.icon}.png`)"
+                                            class="skill-logo"
+                                        ></v-img>
+                                    </v-btn>
+                                </v-hover>
+                            </template>
+                            <span class="d-none">My tooltip</span>
+                        </v-tooltip>
+                    </div>
                 </v-col>
             </v-row>
         </v-container>
